6990f47d1860b626e969ebacce930c6d1a814f06
unknown <musicsouzl@gmail.com>
2012-02-23 19:26:08 -0800

    Solution to problem 3

---
+++ prob3.js
@@ -0,0 +1,53 @@
+﻿/*
+Problem 3
+The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143 ?
+*/ 
+
+function sqrt_root(N) {
+  var oldguess = -1,
+			guess = 1;	
+  while(Math.abs(guess - oldguess) > 1) { // Take avg of N and N/R till both converge
+	oldguess = guess;
+	guess = (guess + N/guess) / 2;		
+  }	
+  return guess;
+}
+                                                                                            
+function isPrime(N) {
+  var root = Math.ceil(sqrt_root(N));
+  for(var i = 2; i < root; i++) {
+    if (N%i === 0) return false;
+  }
+  return true;
+}
+
+function findLargestPrime(N) {
+ 
+ if(isPrime(N)) {
+   return N;
+ }
+ var root = Math.ceil(sqrt_root(N)), // Log N
+     factors = [],
+     largestPrimeFactor = 1, factor;
+    
+ if (N%2 === 0) factors.push(2);
+ 
+ for(var i = 3; i < root; i = i+2) {
+   if(N % i === 0) factors.push(i); // Log N
+ }
+
+ factors.forEach(function(number) {  
+   factor = N /number; 
+   if(isPrime(number)) {   
+     largestPrimeFactor = largestPrimeFactor > number? largestPrimeFactor : number;
+   }    
+   
+   if(isPrime(factor)) {
+     largestPrimeFactor = largestPrimeFactor > factor? largestPrimeFactor : factor;
+   }
+  });  
+ return largestPrimeFactor;
+}
+
+console.log(findLargestPrime(13195));
+console.log(findLargestPrime(600851475143));

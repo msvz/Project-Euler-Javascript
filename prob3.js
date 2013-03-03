bea10b6caf66e74640aa85e1c4e9ec4ff76ffb77
msvz <musicsouzl@gmail.com>
2013-03-02 19:33:23 -0800

    Removed unnecessary if statement. Prob 3

--- prob3.js
+++ prob3.js
@@ -1,17 +1,17 @@
 ﻿/*
 Problem 3
 The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143 ?
-*/ 
+*/
 
 function sqrt_root(N) {
-  var oldguess = -1, guess = 1;	
+  var oldguess = -1, guess = 1;
   while(Math.abs(guess - oldguess) > 1) { // Take avg of N and N/R till both converge
 	oldguess = guess;
-	guess = (guess + N/guess) / 2;		
-  }	
+	guess = (guess + N/guess) / 2;
+  }
   return guess;
 }
-                                                                                            
+
 function isPrime(N) {
   var root = Math.ceil(sqrt_root(N));
   for(var i = 2; i < root; i++) {
@@ -21,30 +21,27 @@ function isPrime(N) {
 }
 
 function findLargestPrime(N) {
- 
+
  if(isPrime(N)) {
-   return N;
+   return N; // return if N is prime.
  }
  var root = Math.ceil(sqrt_root(N)), // Log N
      factors = [],
      largestPrimeFactor = 1, factor;
-    
+
  if (N%2 === 0) factors.push(2);
- 
+
+ // Store all factors
  for(var i = 3; i < root; i = i+2) {
    if(N % i === 0) factors.push(i); // Log N
  }
 
- factors.forEach(function(number) {  
-   factor = N /number; 
-   if(isPrime(number)) {   
+ //
+ factors.forEach(function(number) {
+   if(isPrime(number)) {
      largestPrimeFactor = largestPrimeFactor > number? largestPrimeFactor : number;
-   }    
-   
-   if(isPrime(factor)) {
-     largestPrimeFactor = largestPrimeFactor > factor? largestPrimeFactor : factor;
    }
-  });  
+  });
  return largestPrimeFactor;
 }
 
